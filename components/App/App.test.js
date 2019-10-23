20aa283c91bcfa0091bb460220375a9c05e25be9
Alan Turing <freemasl.sf@gmail.com>
2019-10-22 20:34:36 -0600

    Update App test to a snapshot test

--- components/App/App.test.js
+++ components/App/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import App from './App';
+import { shallow } from 'enzyme';
 
 describe('App', () => {
-
-  it('should pass this test', () => {
-    expect(true).toEqual(true);
+  it('should create a snapshot of the component with the correct data passed through', () => {
+    expect(shallow(<App />)).toMatchSnapshot();
   });
-
 });
