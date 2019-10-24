c94d497b488e32d9cf4c3ff776cdd057bb156b20
Katie Williams <kawilliams8@gmail.com>
2019-10-24 16:51:31 -0600

    Add handleSubmit back into Contact

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -12,30 +12,44 @@ export default class Contact extends Component {
     };
   }
 
+  handleSubmit = () => {
+    const { navigation } = this.props;
+    navigation.navigate("ContactList");
+    this.setState({
+      make: "",
+      model: "",
+      year: "",
+      color: "",
+      license_plate: ""
+    });
+  };
+
   render() {
     return (
       <View style={styles.contactContainer}>
         <Text>Contact Page</Text>
-        <TextInput 
+        <TextInput
           placeholder="name"
           style={styles.input}
-          onChangeText={(text) => this.setState({ name: text })} 
+          onChangeText={text => this.setState({ name: text })}
           value={this.state.name}
-          name="name" />
-        <TextInput 
+          name="name"
+        />
+        <TextInput
           placeholder="phone"
           style={styles.input}
-          onChangeText={(text) => this.setState({ phone: text })}
+          onChangeText={text => this.setState({ phone: text })}
           value={this.state.phone}
-          name="phone" />
-        <TextInput 
+          name="phone"
+        />
+        <TextInput
           placeholder="email"
           style={styles.input}
-          onChangeText={(text) => this.setState({ email: text })}
+          onChangeText={text => this.setState({ email: text })}
           value={this.state.email}
-          name="email" />
-        <TouchableOpacity style={styles.updateBtn}
-          onPress={this.handleSubmit} >
+          name="email"
+        />
+        <TouchableOpacity style={styles.updateBtn} onPress={this.handleSubmit}>
           <Text style={styles.updateBtnText}>Update</Text>
         </TouchableOpacity>
       </View>
