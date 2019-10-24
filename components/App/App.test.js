5252844f28b58bbd87996df7f347479904cb8912
Alan Turing <freemasl.sf@gmail.com>
2019-10-23 20:02:57 -0600

    Add snapshot test for App

--- components/App/App.test.js
+++ components/App/App.test.js
@@ -1,9 +1,16 @@
+import 'react-native';
 import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+jest.mock('../../navigation/DrawerNavigator/DrawerNavigator', () => 'DrawerNavigator');
 
 describe('App', () => {
-
-  it('should pass this test', () => {
-    expect(true).toEqual(true);
+  it('should match the snapshot with all of the data passed through', () => {
+    expect(shallow(<App />)).toMatchSnapshot();
   });
-
 });
