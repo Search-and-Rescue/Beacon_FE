cc1169a5a7333985a4f6f019452497fa0ae0a6d5
Alan Turing <freemasl.sf@gmail.com>
2019-10-24 15:58:45 -0600

    Add basic styling to vehicle form

---
+++ components/Vehicle/styles.js
@@ -0,0 +1,25 @@
+const styles = {
+  vehicleContainer: {
+    flex: 1,
+    alignItems: "center"
+  },
+  input: {
+    backgroundColor: "#fff",
+    borderColor: "black",
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 10,
+    width: "80%"
+  },
+  updateBtn: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    width: "80%"
+  },
+  updateBtnText: {
+    textAlign: "center",
+  }
+}
+
+export default styles;
