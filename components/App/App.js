5f1f0829f83da10440632fa27282ecf6258c67b5
Katie Williams <kawilliams8@gmail.com>
2019-10-24 15:34:35 -0600

    Add form to add/edit a gear item, with basic styling

--- components/App/App.js
+++ components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { AppContainer } from '../../navigation/Navigator';
+import { AppContainer } from '../../navigation/navigator';
 
 
 class App extends Component {
