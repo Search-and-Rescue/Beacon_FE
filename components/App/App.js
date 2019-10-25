11ba53f0e6eec33c8d2da8fe0d813bf9aa224f72
Alan Turing <freemasl.sf@gmail.com>
2019-10-24 21:07:39 -0600

    Setup App component to run the Provider and create the store

--- components/App/App.js
+++ components/App/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 import { AppContainer } from '../../navigation/navigator';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { rootReducer } from '../../reducers';
+
+const store = createStore(rootReducer);
 
 
 class App extends Component {
   render() {
-    return <AppContainer />;
+    return (
+      <Provider store={store}>
+        <AppContainer />;
+      </Provider>
+    )
   }
 }
 export default App;
