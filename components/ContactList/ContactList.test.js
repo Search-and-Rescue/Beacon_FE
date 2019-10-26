39c3035af21e057a562253b4dfd0fc78018a7019
Alan Turing <freemasl.sf@gmail.com>
2019-10-26 13:18:03 -0600

    Update snapshot for WelcomeScreen to only test the component

--- components/ContactList/ContactList.test.js
+++ components/ContactList/ContactList.test.js
@@ -1,11 +1,13 @@
 import 'react-native';
 import React from 'react';
 import { shallow } from 'enzyme';
-import ContactList from './ContactList';
+import { ContactList } from './ContactList';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+jest.mock('react-native-gesture-handler', () => 'TouchableOpacity');
 
 describe('ContactList', () => {
   it('should match the snapshot with all of the data passed through', () => {
--- components/WelcomeScreen/WelcomeScreen.test.js
+++ components/WelcomeScreen/WelcomeScreen.test.js
@@ -1,14 +1,14 @@
 import 'react-native';
 import React from 'react';
 import { shallow } from 'enzyme';
-import WelcomeScreen from './WelcomeScreen';
+import { LoginScreen } from './WelcomeScreen';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
-describe('WelcomeScreen', () => {
+describe('LoginScreen', () => {
   it('should match the snapshot with all of the data passed through', () => {
-    expect(shallow(<WelcomeScreen />)).toMatchSnapshot();
+    expect(shallow(<LoginScreen />)).toMatchSnapshot();
   });
 });
