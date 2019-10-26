1116e879305b618743367ea7f77fc93cacc04258
Katie Williams <kawilliams8@gmail.com>
2019-10-26 12:26:02 -0600

    Pass one GearList item down to Gear, display in the update form

--- components/GearList/GearList.js
+++ components/GearList/GearList.js
@@ -39,15 +39,66 @@ export default class GearList extends Component {
         id: 6,
         item_name: "Snowshoes",
         comments: "Atlas carbon fiber backcountry"
+      },
+      {
+        id: 7,
+        item_name: "Sleeping Bag",
+        comments: "REI 0 degree down, hot pink"
+      },
+      {
+        id: 8,
+        item_name: "Hiking boots",
+        comments: "Salomon size 9 winter boots"
+      },
+      {
+        id: 9,
+        item_name: "Snowshoes",
+        comments: "Atlas carbon fiber backcountry"
+      },
+      {
+        id: 10,
+        item_name: "Sleeping Bag",
+        comments: "REI 0 degree down, hot pink"
+      },
+      {
+        id: 11,
+        item_name: "Hiking boots",
+        comments: "Salomon size 9 winter boots"
+      },
+      {
+        id: 12,
+        item_name: "Snowshoes",
+        comments: "Atlas carbon fiber backcountry"
+      },
+      {
+        id: 13,
+        item_name: "Sleeping Bag",
+        comments: "REI 0 degree down, hot pink"
+      },
+      {
+        id: 14,
+        item_name: "Hiking boots",
+        comments: "Salomon size 9 winter boots"
+      },
+      {
+        id: 15,
+        item_name: "Snowshoes",
+        comments: "Atlas carbon fiber backcountry"
       }
     ];
+
+    const editItem = item => {
+      this.props.navigation.navigate("Gear", 
+      { gear: item }
+      );
+    };
 
     const itemLinks = gearItems.map(gear => {
       return (
         <TouchableOpacity
           key={gear.id}
           style={styles.gearLink}
-          onPress={() => this.props.navigation.navigate("Gear")}
+          onPress={() => editItem(gear)}
         >
           <Text style={styles.gearName}>{gear.item_name}</Text>
         </TouchableOpacity>
