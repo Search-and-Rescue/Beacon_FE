201790e63ad2e9b7ec5928d8001a0e963f1c6ecc
Katie Williams <kawilliams8@gmail.com>
2019-10-25 22:57:45 -0600

    Add contact and vehicle to state, add methods and button

--- components/Trip/Trip.js
+++ components/Trip/Trip.js
@@ -17,21 +17,39 @@ class Trip extends Component {
     this.state = {
       experience_level: 0,
       experience_modal: false,
-      emergency_contact: "",
+      contact: "",
       contact_modal: false,
       vehicle: "",
       vehicle_modal: false
-    }
+    };
   }
 
-  setExperience = (level) => {
-    this.setState({experience_level: level});
+  setExperience = level => {
+    this.setState({ experience_level: level });
     this.toggleExperienceModal();
-  } 
+  };
 
   toggleExperienceModal = () => {
-    this.setState({experience_modal: !this.state.experience_modal});
-  }
+    this.setState({ experience_modal: !this.state.experience_modal });
+  };
+
+  setContact = id => {
+    this.setState({ contact: id });
+    this.toggleContactModal();
+  };
+
+  toggleContactModal = () => {
+    this.setState({ contact_modal: !this.state.contact_modal });
+  };
+
+  setVehicle = id => {
+    this.setState({ vehicle: id });
+    this.toggleVehicleModal();
+  };
+
+  toggleVehicleModal = () => {
+    this.setState({ vehicle_modal: !this.state.vehicle_modal });
+  };
 
   render() {
     const experienceLevels = [
@@ -49,35 +67,88 @@ class Trip extends Component {
       }
     ];
 
+    const contacts = [
+      {
+        name: "Mom",
+        id: 1
+      },
+      {
+        name: "Dad",
+        id: 2
+      },
+      {
+        name: "John",
+        id: 3
+      },
+      {
+        name: "Jane",
+        id: 4
+      }
+    ];
+
+    const vehicles = [
+      {
+        make: "Toyota",
+        model: "Tacoma 1",
+        id: 1
+      },
+      {
+        make: "Toyota",
+        model: "Tacoma 2",
+        id: 2
+      },
+      {
+        make: "Toyota",
+        model: "Tacoma 3",
+        id: 3
+      },
+      {
+        make: "Toyota",
+        model: "Tacoma 4",
+        id: 4
+      }
+    ];
+
     return (
       <View style={styles.tripContainer}>
         <Text>State is {this.state.experience_level}</Text>
-        <Button 
+        <Button
           onPress={() => this.toggleExperienceModal()}
-          title={"Choose Experience Level"}
+          title={"Choose Experience Level: "}
+        ></Button>
+        <Text>State is {this.state.contact}</Text>
+        <Button
+          onPress={() => this.toggleContactModal()}
+          title={"Choose Emergency Contact: "}
+        ></Button>
+        <Text>State is {this.state.vehicle}</Text>
+        <Button
+          onPress={() => this.toggleVehicleModal()}
+          title={"Choose Vehicle: "}
         ></Button>
         <Modal
-          animationType={'slide'}
+          animationType={"slide"}
           visible={this.state.experience_modal}
           transparent={true}
           onRequestClose={() => console.log("close requested")}
         >
           <View style={styles.pickerView}>
-            <Text style={styles.experienceHeading}>Select Experience Level:</Text>
+            <Text style={styles.experienceHeading}>
+              Select Experience Level:
+            </Text>
             {experienceLevels.map((level, index) => {
-                return <TouchableHighlight
+              return (
+                <TouchableHighlight
                   key={index}
                   style={styles.experienceButton}
                   onPress={() => this.setExperience(level.value)}
-                  >
+                >
                   <Text>{level.title}</Text>
-                  </TouchableHighlight>
+                </TouchableHighlight>
+              );
             })}
           </View>
         </Modal>
-        <Text>Temp Emergency Contact</Text>
-        <Text>Temp Gear</Text>
-        <Text>Temp Vehicle </Text>
         <TextInput placeholder="Starting point" style={styles.input} />
         <TextInput placeholder="Ending point" style={styles.input} />
         <TextInput placeholder="Start date" style={styles.input} />
