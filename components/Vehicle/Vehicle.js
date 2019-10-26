daecddd9d1e27e3c3f63d01f6afee4baccc934d2
Katie Williams <kawilliams8@gmail.com>
2019-10-26 14:53:56 -0600

    Pass vehicle props to Vehicle

--- components/Vehicle/Vehicle.js
+++ components/Vehicle/Vehicle.js
@@ -3,14 +3,16 @@ import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import styles from "./styles";
 
 class Vehicle extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    const { vehicle } = this.props.navigation.state.params;
     this.state = {
-      make: "",
-      model: "",
-      year: "",
-      color: "",
-      license_plate: ""
+      make: vehicle.make || "",
+      model: vehicle.model || "",
+      year: vehicle.year || "",
+      color: vehicle.color || "",
+      license_plate: vehicle.license_plate || "",
+      state: vehicle.state || ""
     };
   }
 
@@ -22,50 +24,67 @@ class Vehicle extends Component {
       model: "",
       year: "",
       color: "",
-      license_plate: ""
+      license_plate: "",
+      state: ""
     });
   }
 
   render() {
     return (
       <View style={styles.vehicleContainer}>
-        <Text>Vehicle Page</Text>
-        <TextInput 
+        <Text>Make</Text>
+        <TextInput
           placeholder="make"
           style={styles.input}
-          onChangeText={(text) => this.setState({ make: text })}
+          onChangeText={text => this.setState({ make: text })}
           value={this.state.make}
-          name="make" />
+          name="make"
+        />
+        <Text>Model</Text>
         <TextInput
           placeholder="model"
           style={styles.input}
-          onChangeText={(text) => this.setState({ model: text })}
+          onChangeText={text => this.setState({ model: text })}
           value={this.state.model}
-          name="model" />
+          name="model"
+        />
+        <Text>Year</Text>
         <TextInput
           placeholder="year"
           style={styles.input}
-          onChangeText={(text) => this.setState({ year: text })}
-          value={this.state.year}
-          name="year" />
-        <TextInput 
+          onChangeText={text => this.setState({ year: text })}
+          value={String(this.state.year)}
+          name="year"
+        />
+        <Text>Color</Text>
+        <TextInput
           placeholder="color"
           style={styles.input}
-          onChangeText={(text) => this.setState({ color: text })}
+          onChangeText={text => this.setState({ color: text })}
           value={this.state.color}
-          name="color" />
+          name="color"
+        />
+        <Text>License Plate</Text>
         <TextInput
           placeholder="license plate"
           style={styles.input}
-          onChangeText={(text) => this.setState({ license_plate: text })}
+          onChangeText={text => this.setState({ license_plate: text })}
           value={this.state.license_plate}
-          name="license_plate" />
-        <TouchableOpacity style={styles.updateBtn}
-          onPress={this.handleSubmit} >
+          name="license_plate"
+        />
+        <Text>License State</Text>
+        <TextInput
+          placeholder="license state"
+          style={styles.input}
+          onChangeText={text => this.setState({ state: text })}
+          value={this.state.state}
+          name="state"
+        />
+        <TouchableOpacity style={styles.updateBtn} onPress={this.handleSubmit}>
           <Text style={styles.updateBtnText}>Update</Text>
         </TouchableOpacity>
       </View>
-    )
+    );
   }
 }
 
