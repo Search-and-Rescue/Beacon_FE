1d432efef135972f9073c0c224ad0253db35f0ef
Alan Turing <freemasl.sf@gmail.com>
2019-10-26 13:07:25 -0600

    Display the list of existing emegency contacts

--- components/ContactList/ContactList.js
+++ components/ContactList/ContactList.js
@@ -1,27 +1,46 @@
 import React, { Component } from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, ScrollView } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { connect } from 'react-redux';
 import styles from "./styles";
 
-export default class ContactList extends Component {
+export class ContactList extends Component {
   constructor(props) {
     super(props);
   }
 
   render() {
+    const itemLinks = this.props.contacts.map(contact => {
+      return (
+        <TouchableOpacity
+          key={contact.name}
+          style={styles.contactsLink}
+        >
+          <Text style={styles.contactsName}>{contact.name}</Text>
+        </TouchableOpacity>
+      )
+    });
+
     return (
       <View>
-        <View style={styles.categoryContainer}>
-          <View style={styles.categoryList}></View>
-          <Text>Contact List Page</Text>
-          <View style={styles.listItemBtn}>
-            <Button
-              title="Go to a Contact"
-              style={styles.addItemBtn}
-              onPress={() => this.props.navigation.navigate("Contact")}
-            />
-          </View>
+        <View style={styles.contactsListContainer}>
+          <ScrollView style={styles.contactsList}>
+            {itemLinks}
+          </ScrollView>
+          <TouchableOpacity
+            style={styles.addItemBtn}
+            onPress={() => this.props.navigation.navigate("Contact")}
+          >
+            <Text style={styles.addBtnText}>Add an Emergency Contact</Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
   }
 }
+
+export const mapStateToProps = ({ contacts }) => ({
+  contacts
+});
+
+export default connect(mapStateToProps)(ContactList);
---
+++ components/ContactList/styles.js
@@ -0,0 +1,39 @@
+const styles = {
+  contactsListContainer: {
+    alignItems: "center",
+    height: '96%',
+    marginTop: 20
+  },
+  contactsList: {
+    borderWidth: 2,
+    borderColor: "black",
+    flex: 1,
+    height: 300,
+    width: "90%"
+  },
+  contactsLink: {
+    backgroundColor: '#efefef',
+    borderWidth: 1,
+    borderColor: 'black',
+    flex: 1,
+    height: 30,
+    marginVertical: 5
+  },
+  contactsName: {
+    flex: 1,
+    fontSize: 21,
+    height: 20
+  },
+  addItemBtn: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    width: '100%'
+  },
+  addBtnText: {
+    fontSize: 21,
+    textAlign: "center"
+  }
+};
+
+export default styles;
