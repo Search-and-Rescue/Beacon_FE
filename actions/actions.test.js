71578dfe284143bfd00799449873bea8a06eb850
Alan Turing <freemasl.sf@gmail.com>
2019-10-25 22:01:12 -0600

    Test setVehicles action creator

--- actions/actions.test.js
+++ actions/actions.test.js
@@ -14,7 +14,7 @@ describe('actions', () => {
         password: '303 - 4567',
         email: 'katieW@gmail.com'
       }
-    }
+    };
 
     const result = actions.setUser(mockUser);
 
@@ -22,21 +22,41 @@ describe('actions', () => {
   });
 
   it('should have a type of SET_CONTACTS', () => {
-    const mockContact = {
+    const mockContact = [{
       name: 'Sam Freeman',
       password: '303-1111',
       email: 'samF@gmail.com'
-    };
+    }];
     const expectedAction = {
       type: 'SET_CONTACTS',
-      contacts: {
+      contacts: [{
         name: 'Sam Freeman',
         password: '303-1111',
         email: 'samF@gmail.com'
-      }
-    }
+      }]
+    };
 
     const result = actions.setEmergencyContacts(mockContact);
+
+    expect(result).toEqual(expectedAction);
+  });
+
+  it('should have a type of SET_VEHICLES', () => {
+    const mockVehicles = [{
+      make: 'Subaru',
+      model: 'Impreza',
+      year: '2014'
+    }];
+    const expectedAction = {
+      type: 'SET_VEHICLES',
+      vehicles: [{
+        make: 'Subaru',
+        model: 'Impreza',
+        year: '2014'
+      }]
+    };
+
+    const result = actions.setVehicles(mockVehicles);
 
     expect(result).toEqual(expectedAction);
   });
