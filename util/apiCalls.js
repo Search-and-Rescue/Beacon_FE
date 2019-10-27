d736a2be46ff6af79673112c07c60a867db75732
Alan Turing <freemasl.sf@gmail.com>
2019-10-26 18:19:45 -0600

    Get all trips for a specific user

--- util/apiCalls.js
+++ util/apiCalls.js
@@ -81,3 +81,39 @@ export const getGear = async () => {
   const data = await response.json();
   return data.data;
 }
+
+export const getTrips = async () => {
+  const url = 'https://search-and-rescue-api.herokuapp.com/graphql';
+  const query = `query {
+  user(id: 1) {
+    trips{
+      id
+      name
+      startingPoint
+      endingPoint
+      startDate
+      startTime
+      endDate
+      endTime
+      notificationDate
+      notificationTime
+      travelingCompanions
+      }
+    } 
+  }
+}  `
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ query })
+  };
+  const response = await fetch(url, options);
+  if (!response.ok) {
+    throw Error('Error fetching the user\'s trips.')
+  }
+
+  const data = await response.json();
+  return data.data;
+}
