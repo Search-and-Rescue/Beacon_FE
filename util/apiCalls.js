6891193c5e82c65c73dbdcaab0024f96a5ffeee7
Alan Turing <freemasl.sf@gmail.com>
2019-10-26 18:24:12 -0600

    Add all of the profile information for a specific user

--- util/apiCalls.js
+++ util/apiCalls.js
@@ -1,3 +1,43 @@
+export const getUser = async () => {
+  const url = 'https://search-and-rescue-api.herokuapp.com/graphql';
+  const query = `query{
+    user(id: 1){
+      id
+      name
+      email
+      address
+      city
+      state
+      zip
+      phone
+      passwordDigest
+      allergies
+      experienceLevel
+      birthDate
+      weight
+      height
+      hairColor
+      skinColor
+      gender
+      cosarCard
+    }
+  }`
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ query })
+  };
+  const response = await fetch(url, options);
+  if (!response.ok) {
+    throw Error('Error fetching the user\'s emergency contacts.')
+  }
+
+  const data = await response.json();
+  return data.data;
+}
+
 export const getEmergencyContacts = async() => {
   const url = 'https://search-and-rescue-api.herokuapp.com/graphql';
   const query = `query{
