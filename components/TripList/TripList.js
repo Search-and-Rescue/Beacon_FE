67a1d8e50a2c1cda9f7962de212bfc92ccc3f947
Katie Williams <kawilliams8@gmail.com>
2019-10-28 17:49:39 -0600

    Begin functionality to POST the trip status update

--- components/TripList/TripList.js
+++ components/TripList/TripList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Button, Modal, ScrollView, Text, View } from "react-native";
+import { endActiveTrip } from '../../util/apiCalls';
 import { connect } from "react-redux";
 import styles from "./styles";
 import { TouchableOpacity } from "react-native-gesture-handler";
@@ -8,10 +9,15 @@ export class TripList extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      activeTripId: 1,
       active: true,
       button_modal: false
     }
   }
+  
+  deactivateTripStatus = (id) => {
+    
+  } 
 
   toggleButtonModal = () => {
     this.setState({ button_modal: !this.state.button_modal });
@@ -45,13 +51,17 @@ export class TripList extends Component {
           >
             <View style={styles.pickerView}>
               <Text style={styles.modalHeading}>Update trip status:</Text>
-              <TouchableOpacity>
+              <TouchableOpacity
+                onPress ={()=> this.deactivateTripStatus(this.state.active)}
+              >
                 <View style={styles.theButton}>
                   <Text style={styles.theButtonText}>I'M BACK</Text>
                   <Text>Stop all emergency notifications</Text>
                 </View>
               </TouchableOpacity>
-              <TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => this.toggleButtonModal()}
+              >
                 <View style={styles.remainActiveButton}>
                   <Text style={styles.remainActiveButtonText}>Remain Active</Text>
                 </View>
