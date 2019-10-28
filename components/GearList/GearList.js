ba05313875004c25607fdb5b55351c00a5a34a1b
Katie Williams <kawilliams8@gmail.com>
2019-10-28 15:16:18 -0600

    Add apiCall in delete gear item, add apiCalls, redux files, mapStateToProps

--- components/GearList/GearList.js
+++ components/GearList/GearList.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
 import { ScrollView, Text, View } from "react-native";
+import { getGear, deleteGearItem } from "../../util/apiCalls";
 import { connect } from "react-redux";
 import styles from "./styles";
+import { setGear } from "../../actions";
+import { bindActionCreators } from "redux";
 
 export class GearList extends Component {
   constructor(props) {
     super(props);
   }
 
-  deleteItem = id => {
-    console.log('in delete gear item', id)
+  deleteItem = async id => {
+    await deleteGearItem(id);
+    const userInfoGear = await getGear();
+    console.log('in del gear item', userInfoGear);
+
   };
 
   render() {
@@ -42,4 +48,7 @@ export const mapStateToProps = ({ gear }) => ({
   gear
 });
 
-export default connect(mapStateToProps)(GearList);
+export const mapDispatchToProps = dispatch =>
+  bindActionCreators({ setGear }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(GearList);
