33db6e6fc9920541798af995aac7386afe61dae4
Alan Turing <freemasl.sf@gmail.com>
2019-10-29 16:32:34 -0600

    Add a case in the currentTrip reducer, which returns null when a current trip is removed

--- reducers/currentTrip.js
+++ reducers/currentTrip.js
@@ -2,6 +2,8 @@ export const currentTrip = (state = null, action) => {
   switch (action.type) {
     case 'SET_CURRENT':
       return action.currentTrip;
+    case 'REMOVE_CURRENT':
+      return null;
     default:
       return state;
   }
