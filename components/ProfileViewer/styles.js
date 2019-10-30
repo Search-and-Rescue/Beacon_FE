fe61cfdc344bcc0eee96ea3fba4666917a359a02
Katie Williams <kawilliams8@gmail.com>
2019-10-30 17:28:12 -0600

    Style the ProfileViewer

--- components/ProfileViewer/styles.js
+++ components/ProfileViewer/styles.js
@@ -1,22 +1,25 @@
 const styles = {
   profileContainer: {
-    borderWidth: 1,
-    borderColor: "black",
+    backgroundColor: "#F0F0F0",
+    borderWidth: 4,
+    borderColor: "#001028",
     flex: 1,
     marginHorizontal: "5%",
     marginVertical: 20,
-    paddingLeft: 5,
     width: "90%"
-  }, 
+  },
   profileHeading: {
-    backgroundColor: '#efefef',
-    fontSize: 21,
-    marginTop: 7,
-    paddingVertical: 2,
+    backgroundColor: "#001028",
+    color: '#F0F0F0',
+    fontSize: 28,
+    marginBottom: 7,
+    paddingLeft: 5,
+    paddingVertical: 2
   },
   profile: {
-    fontSize: 18, 
-    paddingTop: 2
+    fontSize: 21,
+    paddingLeft: 5,
+    paddingBottom: 3
   }
 };
 
