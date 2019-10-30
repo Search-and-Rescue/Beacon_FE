707f2fafebc4454d3fcd8493b024b2ec24981f60
Katie Williams <kawilliams8@gmail.com>
2019-10-29 20:10:24 -0600

    Clear out extra Views, add intial container styling

--- components/ProfileViewer/styles.js
+++ components/ProfileViewer/styles.js
@@ -1,58 +1,10 @@
 const styles = {
   profileContainer: {
     flex: 1,
+    marginHorizontal: '10%',
     marginVertical: 40,
-    alignItems: "center"
-  },
-  modalToggleButton: {
-    backgroundColor: "#efefef",
-    marginVertical: 4,
-    width: "80%"
-  },
-  modalToggleText: {
-    fontSize: 21,
-    paddingVertical: 5,
-    textAlign: "center"
-  },
-  profileFlatList: {
-    width: "80%"
-  },
-  pickerView: {
-    backgroundColor: "#efefef",
-    margin: 5,
-    padding: 20,
-    position: "absolute",
-    bottom: 0,
-    left: 10,
-    right: 10
-  },
-  modalHeading: {
-    fontWeight: "bold",
-    marginBottom: 5
-  },
-  modalButton: {
-    paddingVertical: 10,
-    alignItems: "center"
-  },
-  label: {
-    fontSize: 12
-  },
-  input: {
-    backgroundColor: "#fff",
-    borderColor: "black",
-    borderWidth: 1,
-    marginBottom: 10,
-    padding: 10
-  },
-  updateBtn: {
-    borderColor: "black",
-    borderWidth: 1,
-    padding: 10,
-    width: "80%"
-  },
-  updateBtnText: {
-    textAlign: "center"
+    width: '80%'
   }
-};
+}
 
 export default styles;
