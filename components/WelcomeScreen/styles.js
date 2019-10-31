5701dcd2ba500544caa4b531865e47300eb5511a
Katie Williams <kawilliams8@gmail.com>
2019-10-30 19:11:55 -0600

    Update all snapshot tests for current styling

--- components/WelcomeScreen/styles.js
+++ components/WelcomeScreen/styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   loginContainer: {
     alignItems: "center",
-    backgroundColor: "#F0F0F)",
+    backgroundColor: "#F0F0F0",
     flex: 1,
     justifyContent: "center",
     marginTop: 20
