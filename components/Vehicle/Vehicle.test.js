ad528bb5b470e06c0680e56bdf920e318255bde4
Alan Turing <freemasl.sf@gmail.com>
2019-11-24 15:32:36 -0700

    Test onChangeText for all inputs in the Vehicle form

--- components/Vehicle/Vehicle.test.js
+++ components/Vehicle/Vehicle.test.js
@@ -8,6 +8,7 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
 describe('Vehicle', () => {
+  let wrapper
 
   beforeEach(() => {
     mockVehicle = {
@@ -26,9 +27,41 @@ describe('Vehicle', () => {
       }
     }
   }
+
+    wrapper = shallow(<Vehicle {...mockVehicle} />)
   });
 
   it('should match the snapshot with all of the data passed through', () => {
-    expect(shallow(<Vehicle {...mockVehicle}/>)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should update make state onChangeText of input', () => {
+    wrapper.find('TextInput').at(0).simulate('changeText', 'Subaru')
+    expect(wrapper.state('make')).toEqual('Subaru');
+  });
+
+  it('should update model state onChangeText of input', () => {
+    wrapper.find('TextInput').at(1).simulate('changeText', 'Impreza')
+    expect(wrapper.state('model')).toEqual('Impreza');
+  });
+
+  it('should update year state onChangeText of input', () => {
+    wrapper.find('TextInput').at(2).simulate('changeText', 2014)
+    expect(wrapper.state('year')).toEqual(2014);
+  });
+
+  it('should update color state onChangeText of input', () => {
+    wrapper.find('TextInput').at(3).simulate('changeText', 'blue')
+    expect(wrapper.state('color')).toEqual('blue');
+  });
+
+  it('should update licensePlate state onChangeText of input', () => {
+    wrapper.find('TextInput').at(4).simulate('changeText', 'ABC123')
+    expect(wrapper.state('licensePlate')).toEqual('ABC123');
+  });
+
+  it('should update state\'s state onChangeText of input', () => {
+    wrapper.find('TextInput').at(5).simulate('changeText', 'CO')
+    expect(wrapper.state('state')).toEqual('CO');
   });
 });
