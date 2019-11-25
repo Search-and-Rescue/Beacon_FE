9a248074fd6bac300762504183b52214569ee5e0
Alan Turing <freemasl.sf@gmail.com>
2019-11-25 16:42:19 -0700

    Test mapStateToProps in the Contact compnent

--- components/Contact/Contact.test.js
+++ components/Contact/Contact.test.js
@@ -1,14 +1,23 @@
 import 'react-native';
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Contact } from './Contact';
+import { Contact, mapStateToProps, mapDispatchToProps } from './Contact';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { setEmergencyContacts } from '../../actions';
 
 configure({ adapter: new Adapter() });
 
 describe('Contact', () => {
   let wrapper
+
+  const userMock = {
+    id: 1,
+    name: 'Sam Freeman'
+  }
+  const mockState = {
+    user: userMock
+  }
 
   beforeEach(() => {
     mockContact = {
@@ -46,5 +55,15 @@ describe('Contact', () => {
   it('should update email state onChangeText of input', () => {
     wrapper.find('TextInput').at(2).simulate('changeText', 'test@email.com')
     expect(wrapper.state('email')).toEqual('test@email.com');
+  });
+
+  it('mapStateToProps should grab the props it needs', () => {
+    const expected = {
+      user: userMock
+    }
+
+    const mappedProps = mapStateToProps(mockState);
+
+    expect(mappedProps).toEqual(expected);
   });
 });
